9c029fe5d4c28b195bd144b575edfb040571c101
PedroLeiva <pedro.leiva@bandtec.com.br>
2020-10-13 18:59:33 -0300

    arrumando o banco2

--- Banco de Dados/script protótipo 10.10.sql
+++ Banco de Dados/script protótipo 10.10.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE phos4;
 use phos4;
 
-CREATE TABLE login (
+CREATE TABLE Login_empresa (
 	id_login int  primary key auto_increment,
     login_empresa varchar(40),
     senha varchar(70),
@@ -9,7 +9,7 @@ CREATE TABLE login (
     fkcadastro int,
 	);
 
- CREATE TABLE empresa_cadastro (
+ CREATE TABLE Empresa_cadastro (
 	id_cadastro  int primary key auto_increment, 
     nome_empresa varchar(70),
     telefone varchar(11),
@@ -17,10 +17,10 @@ CREATE TABLE login (
     endereço varchar(100),
     cnpj char(14),
     fklogin int,
-	foreign key(fklogin) references login_empresa (id_login)
+	foreign key(fklogin) references Login_empresa (id_login)
     ) auto_increment = 100;
 
-CREATE TABLE luminosidade (
+CREATE TABLE Luminosidade (
 		id_luminosidade int primary key auto_increment,
 		luminosidade_local varchar(40),
 		luminosidade_ideal decimal(6,2),
@@ -28,7 +28,7 @@ CREATE TABLE luminosidade (
 		variacao_luminosidade decimal(6,2),
         data_luminosidade date
 		fkcadastro int,
-		foreign key(fkempresa) references empresa_cadastro (id_cadastro)
+		foreign key(fkempresa) references Empresa_cadastro (id_cadastro)
 
 
     ) auto_increment = 1000;
@@ -38,7 +38,7 @@ CREATE TABLE luminosidade (
 SELECT *
 FROM login;
 
-INSERT INTO luminosidade 
+INSERT INTO Luminosidade 
 VALUES(null,'1000lux','980lux','20lux', '1990-05-18'),
       (null,'1100lux','950lux','150lux', '2020-10-10'),
       (null,'1200lux','1000lux','200lux','2010-10-12'),
@@ -47,7 +47,7 @@ VALUES(null,'1000lux','980lux','20lux', '1990-05-18'),
       (null,'750lux','540lux','100lux','2003-04-15'),
       (null,'650lux','850lux','50lux','1998-06-30');
       
-INSERT INTO empresa_cadastro 
+INSERT INTO Empresa_cadastro 
 VALUES(null,'GOOGLE','11987388111','google@gmail.com', 'rua cinco','63512489000121', 1000),
       (null,'iutube','21923421421','arebaba@hotmail.com','rua J','26254897000151', 1001),
       (null,'Puma','51977686316','puma@gmail.com','rua augusta', '36598421000119', 1002),
@@ -56,7 +56,7 @@ VALUES(null,'GOOGLE','11987388111','google@gmail.com', 'rua cinco','635124890001
       (null,'McDonalds','1198754432','mcdonalds@mcdonalds.com', 'rua joaquim marra', '05125461000125', 1005),
       (null,'GetNet','1153462455','getnet@getnet.com', 'rua coracao de maça','12345688000135', 1006);
 
-INSERT INTO login 
+INSERT INTO Login_empresa
 VALUES(null,'natanista','@2e1oUdhu8QWQ80BWD', 1, 100),
       (null,'LuizGsilva','ghghhghg', 0, 101),
       (null,'ViniOliveira','vinivini321', 0, 102),
@@ -66,6 +66,6 @@ VALUES(null,'natanista','@2e1oUdhu8QWQ80BWD', 1, 100),
       (null,'MatheusDaniel','@123456', 0, 106);
 
 
-select * from empresa_cadastro, luminosidade, login where fkcadastro = id_cadastro and fkluminosidade = id_luminosidade;
+select * from Empresa_cadastro, Luminosidade, Login_empresa where fklogin = id_login and fkcadastro = id_cadastro;
 
       
