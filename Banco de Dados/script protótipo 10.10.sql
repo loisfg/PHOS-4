53f1c39d0ce6d200124b8f1164e207cc97d75ce2
PedroLeiva <pedro.leiva@bandtec.com.br>
2020-10-27 12:34:37 -0200

    ppt e bd

--- Banco de Dados/script protótipo 10.10.sql
+++ Banco de Dados/script protótipo 10.10.sql
@@ -1,14 +1,6 @@
 CREATE DATABASE phos4;
 use phos4;
 
-
-CREATE TABLE empresa_login (
-	id_login int  primary key auto_increment,
-    login_empresa varchar(40),
-    senha varchar(70),
-    adm_usuario int check (adm_usuario = 0 or adm_usuario = 1)
-    );
-
 CREATE TABLE empresa_cadastro (
 	id_cadastro  int primary key auto_increment, 
     nome_empresa varchar(70),
@@ -16,9 +8,16 @@ CREATE TABLE empresa_cadastro (
     email varchar(255),
     enderço varchar(100),
     CNPJ char(14),
-    fklogin int,
-	foreign key(fklogin) references empresa_login (id_login)
     ) auto_increment= 100;
+
+	   CREATE TABLE empresa_login (
+	id_login int  primary key auto_increment,
+    login_empresa varchar(40),
+    senha varchar(70),
+    adm_usuario int check (adm_usuario = 0 or adm_usuario = 1),
+	fkempresa_cadastro int,
+	foreign key(fkempresa_cadastro) references empresa_cadastro (id_cadastro)
+    );
 
 	create table Ambiente(
 	id_Ambiente int primary key auto_increment,
@@ -28,7 +27,7 @@ CREATE TABLE empresa_cadastro (
     foreign key(fkcadastro) references empresa_cadastro (id_cadastro)
 	)auto_increment = 1000;
 
-    
+
  CREATE TABLE Sensor (
 		id_Sensor int primary key auto_increment,
         Nome varchar(45),
@@ -37,23 +36,24 @@ CREATE TABLE empresa_cadastro (
     ) auto_increment= 5000;
       
 
-INSERT INTO empresa_login 
-VALUES(null,'natanista','@2e1oUdhu8QWQ80BWD', 1),
-      (null,'LuizGsilva','ghghhghg', 0),
-      (null,'ViniOliveira','vinivini321', 0 ),
-      (null, 'LuisFRocha','lflflflf', 0),
-      (null,'Carlos','carlos15', 0),
-      (null,'ThallesBuso','thallesb', 0),
-      (null,'MatheusDaniel','@123456', 0);
       
       INSERT INTO empresa_cadastro 
-VALUES(null,'GOOGLE','11987388111','google@gmail.com', 'rua cinco','63512489000121', 1),
-      (null,'iutube','21923421421','arebaba@hotmail.com','rua J','26254897000151', 1),
-      (null,'Puma','51977686316','puma@gmail.com','rua augusta', '36598421000119', 2),
-      (null,'Git','1199878787','git@hub.com', 'rua de melo', '23654789000125', 3),
-      (null,'amazon','19933142','amazon@amazon.com','rua hadock', '54236987000141', 4),
-      (null,'McDonalds','1198754432','mcdonalds@mcdonalds.com', 'rua joaquim marra', '05125461000125', 5),
-      (null,'GetNet','1153462455','getnet@getnet.com', 'rua coracao de maça','12345688000135', 6);
+VALUES(null,'GOOGLE','11987388111','google@gmail.com', 'rua cinco','63512489000121' ),
+      (null,'iutube','21923421421','arebaba@hotmail.com','rua J','26254897000151'),
+      (null,'Puma','51977686316','puma@gmail.com','rua augusta', '36598421000119'),
+      (null,'Git','1199878787','git@hub.com', 'rua de melo', '23654789000125'),
+      (null,'amazon','19933142','amazon@amazon.com','rua hadock', '54236987000141'),
+      (null,'McDonalds','1198754432','mcdonalds@mcdonalds.com', 'rua joaquim marra', '05125461000125'),
+      (null,'GetNet','1153462455','getnet@getnet.com', 'rua coracao de maça','12345688000135');
+
+	  INSERT INTO empresa_login 
+VALUES(null,'natanista','@2e1oUdhu8QWQ80BWD', 1,100),
+      (null,'LuizGsilva','ghghhghg', 1,101),
+      (null,'ViniOliveira','vinivini321', 1,102),
+      (null, 'LuisFRocha','lflflflf', 1,103),
+      (null,'Carlos','carlos15', 1,104),
+      (null,'ThallesBuso','thallesb', 1,105),
+      (null,'MatheusDaniel','@123456',1, 106);
       
       INSERT INTO Ambiente 
 VALUES(null,'Sala1C GOOGLE','centro',100),
@@ -93,6 +93,6 @@ VALUES(5000,'701.45','2020-10-17 12:30:00.000'),
       (5006,'689.22','2020-10-17 12:40:00.000');
 
 
-select * from empresa_login, empresa_cadastro, Ambiente, Sensor, leitura where fklogin = id_login and fkcadastro = id_cadastro and fkambiente = id_Ambiente and fksensor = id_Sensor;
+select * from empresa_login, empresa_cadastro, Ambiente, Sensor, leitura where fkcadastro = id_cadastro and fkambiente = id_Ambiente and fksensor = id_Sensor;
 
       
