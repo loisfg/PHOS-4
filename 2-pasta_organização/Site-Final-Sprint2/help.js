d2f4a1a3d14ca6de5569d4a06f1485c2664eeccc
matheusmarques17 <matheus.marques@bandtec.com.br>
2020-11-19 19:41:16 -0300

    inclusão ferramenta de help desk

---
+++ 2-pasta_organização/Site-Final-Sprint2/help.js
@@ -0,0 +1,9 @@
+<script> (function (d, w, c)  if(!d.getElementById("spd-busns-spt")) var n = d.getElementsByTagName('script')[0], s =
+ d.createElement('script'); var loaded = false; s.id = "spd-busns-spt"; s.async = "async"; s.setAttribute("data-self-init", "false"); 
+ s.setAttribute("data-init-type", "opt"); s.src = 'https://cdn.freshbots.ai/assets/share/js/freshbots.min.js'; 
+ s.setAttribute("data-client", "0ce530b5723ed6f60ef379e818824f464bdb5fc1"); s.setAttribute("data-bot-hash", "1fa540444b7e9a60884a6e13573c06b143fe6c29");
+  s.setAttribute("data-env", "prod"); s.setAttribute("data-region", "us"); if (c) { s.onreadystatechange = s.onload = function () { if (!loaded) { c(); }
+   loaded = true; } } n.parentNode.insertBefore(s, n);  ) (document, window, function () { Freshbots.initiateWidget({ autoInitChat: false, getClientParams:
+    function () { return {"cstmr::eml":"fernando.brandao@bandtec.com.br","cstmr::phn":"(11)94996-8472","cstmr::nm":"Fernando Brandão"}; } }, function(successResponse) { }, 
+    function(errorResponse) { }) });
+ </script>
